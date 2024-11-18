a02f5a8b772a1c4ed215b71f4f013079f24f34ff
nemethd01 <120584904+nemethd01@users.noreply.github.com>
2024-11-18 20:06:28 +0100

    Add files via upload

--- udemyCppGame/UdemyCppGame11.cpp
+++ udemyCppGame/UdemyCppGame11.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory> // contains smart pointers
 
 // for stack memory example
 void printInt(int val)
@@ -11,6 +12,18 @@ void printInt(int val)
 
     int d = 10;
 } // both val and d runs out of scope, they are all removed here
+
+void printIntMemoryLeak(int val)
+{
+    int* c = new int{ val }; // c is a raw pointer
+
+    if (val == 5)
+    {
+        return; // this is causes memory leak, meaning the memory is not deleted or freed, but the whole program has no variable that hold it's address
+    }
+
+    delete c;
+}
 
 int main()
 {
@@ -50,10 +63,36 @@ int main()
     int* heapX = new int[3]; // int variable (heapX) with value 3 is created on the heap, and then it's address is stored in the stack variable 'heapX'(which is a pointer).
 
     delete heapX;
+    heapX = nullptr;
 
     printInt(a);
+
+    printIntMemoryLeak(5); // memory leak
     // heap can be used when need a big storage.
     // the memory not allocated is not automatically removed.
 
     // to manually free the memory you need to call delete on the pointer
+
+    // c++ is dangerous
+    // modern way to use pointers: smart pointers
+
+
+    // unique pointer
+    // unique ptr the usage is very similar to raw pointer. It automatically delete or free the memory when it goes out of scope
+    std::unique_ptr<int> intUniquePtr = std::make_unique<int>(5);
+    std::cout << intUniquePtr << std::endl;
+
+    // it does not allow copy, but allows movement. The std::move transfers ownership of the raw pointer internally to the other one.
+    std::unique_ptr<int> anotherIntUniquePtr = std::move(intUniquePtr); // mostmár az intUniquePtr egy nullptr lett
+
+    std::cout << intUniquePtr << std::endl;
+    if (intUniquePtr)
+    {
+        std::cout << "test" << *intUniquePtr << std::endl;
+    }
+
+
+
+    // shared pointer
+    
 }
